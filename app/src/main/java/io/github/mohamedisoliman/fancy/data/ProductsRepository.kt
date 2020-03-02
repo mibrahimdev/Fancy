decec0d639fe9ffb79e1d27e7ba5906be114bd71
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2020-03-02 17:43:37 +0300

    implement Products repository

--- app/src/main/java/io/github/mohamedisoliman/fancy/data/ProductsRepository.kt
+++ app/src/main/java/io/github/mohamedisoliman/fancy/data/ProductsRepository.kt
@@ -1,6 +1,23 @@
 package io.github.mohamedisoliman.fancy.data
 
+import io.github.mohamedisoliman.fancy.data.entities.Product
+import io.github.mohamedisoliman.fancy.data.remote.DataKickRemote
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 /**
  *
  * Created by Mohamed Ibrahim on 3/2/20.
  */
+class ProductsRepository(private val remote: DataKickRemote) : ProductRepositoryContract {
+
+    override suspend fun fetchProducts(): List<Product> = remote.fetchProducts()
+
+    override suspend fun fetchProductDetails(id: Int) = remote.fetchProductDetails(id)
+
+}
+
+interface ProductRepositoryContract {
+    suspend fun fetchProducts(): List<Product>
+    suspend fun fetchProductDetails(id: Int)
+}
