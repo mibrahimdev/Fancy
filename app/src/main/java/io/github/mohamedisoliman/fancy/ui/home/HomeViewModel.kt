42b24fa3f61d4a3f31b38c088ccaf93ad9c70179
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2020-03-02 18:12:28 +0300

    setup HomeViewModel

--- app/src/main/java/io/github/mohamedisoliman/fancy/ui/home/HomeViewModel.kt
+++ app/src/main/java/io/github/mohamedisoliman/fancy/ui/home/HomeViewModel.kt
@@ -1,7 +1,39 @@
 package io.github.mohamedisoliman.fancy.ui.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.github.mohamedisoliman.fancy.data.entities.Product
+import io.github.mohamedisoliman.fancy.domain.RetrieveProducts
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HomeViewModel(private val retrieveProducts: RetrieveProducts) : ViewModel() {
+
+    private val productsList = MutableLiveData<List<Product>>()
+    fun productsList(): LiveData<List<Product>> = productsList
+
+    private val progress = MutableLiveData<Boolean>()
+    fun progress(): LiveData<Boolean> = progress
+
+    private val error = MutableLiveData<String>()
+    fun error(): LiveData<String> = error
+
+    init {
+
+    }
+
+    @ExperimentalCoroutinesApi
+    fun initViews() {
+        viewModelScope.launch {
+            progress.postValue(true)
+            val list = retrieveProducts.productsFlow().toList()
+            productsList.postValue(list)
+            progress.postValue(false)
+        }
+    }
+
+
 }
