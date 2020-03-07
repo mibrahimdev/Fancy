ba2f4fd8fe9ebc56d279c8a4a54bfaf38f5ba9f3
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2020-03-07 20:58:31 +0300

    setup details ui

---
+++ app/src/main/java/io/github/mohamedisoliman/fancy/ui/CollapsingToolbar.kt
@@ -0,0 +1,25 @@
+package io.github.mohamedisoliman.fancy.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.appbar.AppBarLayout
+
+/**
+ *
+ * Created by Mohamed Ibrahim on 3/7/20.
+ */
+
+class CollapsibleToolbar @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : MotionLayout(context, attrs, defStyleAttr), AppBarLayout.OnOffsetChangedListener {
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+        progress = -verticalOffset / appBarLayout?.totalScrollRange?.toFloat()!!
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        (parent as? AppBarLayout)?.addOnOffsetChangedListener(this)
+    }
+}
